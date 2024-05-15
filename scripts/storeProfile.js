f24e3bce9f9574461e6d4fad0a88de92de4ffa25
laystr <bomby88888@gmail.com>
2024-05-15 23:45:00 +0700

    Fixed footer

--- scripts/storeProfile.js
+++ scripts/storeProfile.js
@@ -135,3 +135,29 @@ function displayProduct(product, productId) {
 
 // Fetch products when the DOM content is loaded
 document.addEventListener('DOMContentLoaded', fetchProducts);
+
+// Insert header and footer
+document.addEventListener("DOMContentLoaded", function () {
+    fetch('header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header').innerHTML = data;
+
+            const hamburger = document.getElementById('hamburger-menu');
+            const navLinks = document.getElementById('nav-links');
+
+            hamburger.addEventListener('click', () => {
+                if (navLinks.style.display === 'flex') {
+                    navLinks.style.display = 'none';
+                } else {
+                    navLinks.style.display = 'flex';
+                }
+            });
+        });
+
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer').innerHTML = data;
+        });
+});
